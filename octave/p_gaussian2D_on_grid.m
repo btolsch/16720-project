1e890ff373bcd924ea774295d81a6d3159a29eeb
Joseph <jmrichar@andrew.cmu.edu>
2015-12-06 23:37:54 -0500

    Fudged a few images.

--- octave/p_gaussian2D_on_grid.m
+++ octave/p_gaussian2D_on_grid.m
@@ -6,6 +6,8 @@ function [p] = p_gaussian2D_on_grid(imsize, mu, var)
     %OUTPUTS
         %p - image of size imsize giving the probability of at each position
     [gridX, gridY] = meshgrid(1:imsize(2), 1:imsize(1));
+    mu = resize(mu, [1,2]);
+    var = resize(var, [2,2]);
     p_flat = mvnpdf([gridX(:), gridY(:)], mu, var);
     p = reshape(p_flat, imsize);
 end
